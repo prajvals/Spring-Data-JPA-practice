ba5ba6df874a117188abb85027da424fe4a176a3
prajvals <prajvaltheking@gmail.com>
2022-09-09 10:36:51 +0530

    Changed the Pom.xml to accomodate 1.8 java and added the student Entity

---
+++ src/main/java/com/example/demo/entity/Student.java
@@ -0,0 +1,39 @@
+package com.example.demo.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(
+        name = "tbl_student",
+        uniqueConstraints = @UniqueConstraint(
+                name = "emailId",
+                columnNames = "email_address"
+        )
+)
+public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "student_sequence")
+    private Long StudentId;
+    private String firstName;
+    private String lastName;
+    @Column(
+            name = "email_address",
+            nullable = false
+    )
+    private String emailId;
+    private String guardianName;
+    private String guardianEmail;
+    private String guardianMobile;
+}
