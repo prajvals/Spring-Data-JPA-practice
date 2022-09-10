1cef8acbe0f946270c62955674e30ac16f0f1c83
prajvals <prajvaltheking@gmail.com>
2022-09-10 09:53:15 +0530

    added update query

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -2,10 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -29,5 +31,13 @@ public interface StudentRepository extends JpaRepository<Student,Long> {
             nativeQuery = true
     )
     List<Student> getStudentByFirstNameNativeQueryNamedParams(@Param("firstName") String firstName);
+
+    @Modifying
+    @Transactional
+    @Query(
+            value = "update tbl_student set first_name = ?1 where email_address = ?2 ",
+            nativeQuery = true
+    )
+    List<Student> updateStudentByFirstNameNativeQuery(String firstName,String emailAdress);
 
 }
